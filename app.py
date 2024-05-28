2caf4fcc4f44c7ce021257a84826de751df1f629
khachungtrinh <68016396+khachungtrinh@users.noreply.github.com>
2024-05-28 11:01:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,10 +112,10 @@ def md_thamkhao(sutta_id: str, sutta_name: str):
     url_vi = 'https://suttacentral.net/{}/vi/minh_chau?lang=en&reference=none&highlight=false'.format(sutta_id)
     url_the = 'https://thebuddhaswords.net/{}/{}.html#content'.format(sutta_book, sutta_id)
     smarkdown('###### ' + sutta_name + '    <sub>' +
-              creat_link('all', url_all) + ' - ' +
               creat_link('en', url_en) + ' - ' +
               creat_link('vi', url_vi) + ' - ' +
-              creat_link('the', url_the) +
+              creat_link('the', url_the) + ' - ' +
+              creat_link('main', url_all) + 
               '</sub>'
              )
 
